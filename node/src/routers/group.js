20a5c9749328329b6be1f0127e108a2e9b720fb0
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2020-09-15 16:18:30 +0300

    patch end point

--- node/src/routers/group.js
+++ node/src/routers/group.js
@@ -27,7 +27,8 @@ router.get('/groups/:id', async (req, res) =>{
     }
 
 
-})
+});
+
 
 router.post('/groups', async (req,res) =>{
     // const group = new Group({
@@ -44,5 +45,48 @@ router.post('/groups', async (req,res) =>{
         res.status(500).send("err");
     }
 });
+
+
+router.patch('/groups/:id', async (req,res) =>{
+    const id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send({
+            error: "bad request non valid Id"
+        });
+    }
+    try{
+        const oldGroup = await Group.findOne({_id: id});
+        
+        const newGroup = new Group({
+            name: oldGroup.name,
+            members: [...oldGroup.members]
+        });
+        newGroup.members.push(req.body)
+
+        const group = await Group.findOneAndUpdate({_id: id}, {"members" : newGroup.members},
+                                                {new: true, runValidators: true});
+        if(!group){
+            return res.status(404).send();
+        }
+        res.status(200).send(group);
+    }
+    catch(err){
+        res.status(400).send(err);
+    }
+
+
+
+    // try{
+    //     const group = new Group(req.body);
+    //     console.log(group)
+    //     await group.save();
+    //     res.status(200).send(group);
+    // }
+    // catch(err){
+    //     res.status(500).send("err");
+    // }
+});
+
 
 module.exports = router;
